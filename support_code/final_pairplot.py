7f548c1ca45c1f708b878032837d7ffdd7c86d1c
PhyAMR <alvaromendezrt@gmail.com>
2026-03-02 13:25:53 +0100

    Fixed the creation of the dataset

--- support_code/final_pairplot.py
+++ support_code/final_pairplot.py
@@ -1,8 +1,10 @@
 # This file creats the html report for the final dataset since jupyter keeps crushing when I try to make it in the notebook
 import pandas as pd
 from pathlib import Path
-from ydata_profiling import ProfileReport
+import seaborn as sns
+import numpy as np
 
+# 1. Definir la ruta base donde están tus carpetas
 def read_nth_generation(path):
     # read and repair header (merge tokens that start with '(' into previous token)
     with open(path, 'r') as f:
@@ -35,5 +37,14 @@ df_final.drop(columns=['c5:theta1', 'c6:theta2', 'c7:SMA(Rsun)', 'c8:ecc','c10:S
        'c23:flag_t3bb', 'c24:flag_evap','c26:ecc(10Hz)'], inplace=True)
 df_final['c0:identifier'] = df_final['c0:identifier'].astype('category')
 
-profile_total = ProfileReport(df_final, title="Profiling Report for total dataset", explorative=True)
-profile_total.to_file("total_report.html")
+df_final_no_id = df_final.drop(columns=['c0:identifier'])
+numeric_cols = df_final_no_id.select_dtypes(include=[np.number]).columns.tolist()
+
+g = sns.pairplot(df_final_no_id, 
+                 vars=numeric_cols, # Only plot these
+                 hue="sys", 
+                 diag_kind="hist", 
+                 palette="Set2")
+g.map_lower(sns.kdeplot, levels=4, color=".2")
+g.fig.suptitle("Pairplot of the final dataset", y=1.02)
+g.savefig("final_pairplot.png", dpi=300)
